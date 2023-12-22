52901b3297c2ed35b61305215dd41ed98e301469
NicoHernandezR <nic.hernandezr@duocuc.cl>
2023-12-22 19:04:29 -0300

    Barra de Progreso Agregada, con algunas mejoras al reproductor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -17,6 +17,7 @@ export class HomePage implements OnInit{
   supabase: SupabaseClient;
   constructor(private audioService: AudioReproducerService) {
     this.supabase = createClient(environment.supaBaseUrl, environment.supaBaseKey);
+
   }
 
   
@@ -31,12 +32,73 @@ export class HomePage implements OnInit{
   actual: any
   prev: any
   next: any
+  titulo: string | null = ''
+  duracion: any = 0
+  progresoSegundos: any = 0
+  progreso: number = 0
+  intervalo: any
+  progresoStr: any
+  duracionStr: any
 
-  ngOnInit(): void {
+
+  async ngOnInit() {
     this.getTotalSongs()
     this.cargarCancionActual()
     this.cargcarCancionPrevia()
-    this.cargarCancionNext()
+    await this.cargarCancionNext()
+    this.actualizarTitulo()
+
+  }
+
+  obtenerProgreso(){
+    this.progresoSegundos = this.player?.seek()
+  }
+
+  obtenerDuracion(){
+    this.duracion = this.player?.duration()
+  }
+
+  calcularProgreso(){
+    this.progreso = this.progresoSegundos / this.duracion
+  }
+
+  formatearAMinutos(secs: number){
+    let minutos = Math.floor(secs / 60);
+    let segundos = Math.floor(secs % 60);
+    let duracionTexto = minutos + ':' + (segundos < 10 ? '0' : '') + segundos;
+    return duracionTexto
+  }
+
+  actualizarContadorSegundos(){
+    this.progresoStr = this.formatearAMinutos(this.progresoSegundos)
+  }
+
+  actualizarDuracionStr(){
+    this.obtenerDuracion()
+    console.log(this.duracion)
+    this.duracionStr = this.formatearAMinutos(this.duracion)
+  }
+
+  barraDeReproduccion() {
+    this.intervalo = setInterval(() => {
+      if(this.player?.playing()){
+        this.obtenerProgreso()
+        this.obtenerDuracion()
+        this.actualizarDuracionStr()
+        if (this.progresoSegundos < this.duracion) {
+          this.calcularProgreso()
+          this.actualizarContadorSegundos()
+        } else{
+          console.log('Termino la cancion')
+          this.siguienteCancion()
+          clearInterval(this.intervalo);
+        }
+      }else if (this.progreso !== 0 && this.duracion !== 0){
+        console.log('Termino la cancion')
+        this.siguienteCancion()
+        clearInterval(this.intervalo);
+      }
+    }, 1000);  // Actualiza cada segundo
   }
 
   async leerArchivo(path: string): Promise<string | Blob>{
@@ -52,17 +114,29 @@ export class HomePage implements OnInit{
     }
   }
   
-  async escribirArchivo(path: string, data: string | Blob): Promise<void> {
+  guardarTitulo(path:string, nom:string | Blob){
+    let nuevoPath = path + 'nom'
+    if (typeof nom === 'string') {
+    localStorage.setItem(nuevoPath, nom)
+    }
+  }
+
+
+  async escribirArchivo(path: string, data: string | Blob, nom: string | Blob): Promise<void> {
     try {
       await Filesystem.writeFile({
         path: path,
         data: data,
         directory: Directory.Data,
       });
+
     } catch (error) {
       console.error(`Error al escribir en el archivo ${path}`, error);
       // Puedes manejar el error de otra manera según tus necesidades
     }
+
+    this.guardarTitulo(path, nom)
+
   }
 
 
@@ -80,14 +154,14 @@ export class HomePage implements OnInit{
   async cargarCancionActual(){
     const { data, error } = await this.supabase
     .from('tabla_mp3')
-    .select('archivo_mp3')
+    .select('archivo_mp3,nombre_archivo')
     .eq('id', this.idActual);
 
     if (!(data && data.length > 0 && data[0]['archivo_mp3'])) {
       return;
     }
-
-    await this.escribirArchivo(this.actSTR, data[0]['archivo_mp3'])
+    
+    await this.escribirArchivo(this.actSTR, data[0]['archivo_mp3'], data[0]['nombre_archivo'])
 
   }
 
@@ -99,14 +173,14 @@ export class HomePage implements OnInit{
 
     const { data, error } = await this.supabase
     .from('tabla_mp3')
-    .select('archivo_mp3')
+    .select('archivo_mp3,nombre_archivo')
     .eq('id', this.idActual - 1);
 
     if (!(data && data.length > 0 && data[0]['archivo_mp3'])) {
       return;
     }
 
-    await this.escribirArchivo(this.prevSTR, data[0]['archivo_mp3'])
+    await this.escribirArchivo(this.prevSTR, data[0]['archivo_mp3'], data[0]['nombre_archivo'])
 
   }
 
@@ -118,33 +192,40 @@ export class HomePage implements OnInit{
 
     const { data, error } = await this.supabase
     .from('tabla_mp3')
-    .select('archivo_mp3')
+    .select('archivo_mp3,nombre_archivo')
     .eq('id', (this.idActual + 1));
 
     if (!(data && data.length > 0 && data[0]['archivo_mp3'])) {
       return;
     }
 
-    await this.escribirArchivo(this.nextSTR, data[0]['archivo_mp3'])
+    await this.escribirArchivo(this.nextSTR, data[0]['archivo_mp3'], data[0]['nombre_archivo'])
 
   }
 
   async comprobar() {
-    const prev = await Filesystem.readFile({      
-      path: 'prevBS64',
-      directory: Directory.Data,}
-    )
-    const actual = await Filesystem.readFile({      
-      path: 'actualBS64',
-      directory: Directory.Data,}
-    )
-    const next = await Filesystem.readFile({      
-      path: 'nextBS64',
-      directory: Directory.Data,}
-    )
-    console.log(prev.data)
-    console.log(actual.data)
-    console.log(next.data)
+    // const prev = await Filesystem.readFile({      
+    //   path: 'prevBS64',
+    //   directory: Directory.Data,}
+    // )
+    // const actual = await Filesystem.readFile({      
+    //   path: 'actualBS64',
+    //   directory: Directory.Data,}
+    // )
+    // const next = await Filesystem.readFile({      
+    //   path: 'nextBS64',
+    //   directory: Directory.Data,}
+    // )
+    // console.log(prev.data)
+    // console.log(actual.data)
+    // console.log(next.data)
+
+    const prev = localStorage.getItem(this.prevSTR + 'nom')
+    const act = localStorage.getItem(this.actSTR + 'nom')
+    const next = localStorage.getItem(this.nextSTR + 'nom')
+    console.log(prev)
+    console.log(act)
+    console.log(next)
   }
 
   async reproducirOpausar(){
@@ -152,13 +233,14 @@ export class HomePage implements OnInit{
     if(this.reproduciendo){
       this.player?.pause()
       this.reproduciendo = false
-      console.log(this.player?.seek())
+      clearInterval(this.intervalo);
       return
     }
 
     if(this.player?.playing){
       this.player?.play()
       this.reproduciendo = true
+      this.barraDeReproduccion()
       return
     }
 
@@ -172,38 +254,56 @@ export class HomePage implements OnInit{
     })
     this.player.play()
     this.reproduciendo = true
-
+    this.barraDeReproduccion()
   }
 
   async siguienteCancion() {
     const actual = await this.leerArchivo(this.actSTR)
-    await this.escribirArchivo(this.prevSTR, actual)
+    const actNom = localStorage.getItem(this.actSTR + 'nom')
+    if(typeof actNom === 'string'){
+      await this.escribirArchivo(this.prevSTR, actual, actNom)
+    }
+
 
     const next = await this.leerArchivo(this.nextSTR)
-    await this.escribirArchivo(this.actSTR, next)
+    const nextNom = localStorage.getItem(this.nextSTR + 'nom')
+    if(typeof nextNom === 'string'){
+      await this.escribirArchivo(this.actSTR, next, nextNom)
+    }
 
     this.idActual = this.idActual + 1;
-    this.player?.stop()
-    this.player = undefined
-    this.reproduciendo = false
-    this.reproducirOpausar()
-    this.cargarCancionNext()
+    this.reiniciarCancion()
+    await this.cargarCancionNext()
+    this.actualizarTitulo()
+    this.actualizarDuracionStr()
     
   }
 
   async previaCancion(){
     const actual = await this.leerArchivo(this.actSTR)
-    await this.escribirArchivo(this.nextSTR, actual)
+    const actNom = localStorage.getItem(this.actSTR + 'nom')
+    if(typeof actNom === 'string'){
+      await this.escribirArchivo(this.nextSTR, actual, actNom)
+    }
 
     const prev = await this.leerArchivo(this.prevSTR)
-    await this.escribirArchivo(this.actSTR, prev)
-
+    const prevNom = localStorage.getItem(this.prevSTR + 'nom')
+    if(typeof prevNom === 'string'){
+      await this.escribirArchivo(this.actSTR, prev, prevNom)
+    }
     this.idActual = this.idActual - 1;
+    this.reiniciarCancion()
+    await this.cargcarCancionPrevia()
+    this.actualizarTitulo()
+    this.actualizarDuracionStr()
+  }
+
+  reiniciarCancion(){
     this.player?.stop()
     this.player = undefined
     this.reproduciendo = false
+    clearInterval(this.intervalo)
     this.reproducirOpausar()
-    this.cargcarCancionPrevia()
   }
 
   validarCambiarNext(){
@@ -214,20 +314,19 @@ export class HomePage implements OnInit{
     return this.idActual !== 1
   }
 
+  actualizarTitulo(){
+    let path = this.actSTR + 'nom'
+    this.titulo = localStorage.getItem(path)
+  }
+
   async playAudio() {
     const { data, error } = await this.supabase
     .from('tabla_mp3')
     .select('archivo_mp3')
     .eq('id', '5');
-
-    console.log("data")
-    console.log(data)
-    console.log("error")
-    console.log(error)
     
     if (data && data.length > 0 && data[0]['archivo_mp3']) {
       let base64String = data[0]['archivo_mp3']
-      console.log(base64String)
 
       let mediaType = "audio/mp3"; // Reemplaza esto con el tipo de medio correcto
 
@@ -261,14 +360,16 @@ export class HomePage implements OnInit{
         });
 
         this.mensaje = fileStat.uri;
-        console.log('Información del archivo:', fileStat);
-        console.log('Archivo MP3 guardado correctamente');
     } catch (error) {
         console.error('Error al obtener información del archivo', error);
     }
   }
 
 
+
+  ngOnDestroy(){
+    localStorage.clear()
+  }
 
 
 }
